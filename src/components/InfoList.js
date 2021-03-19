51b45aea053fdf6bcfcf509edb6e27a5fb636afc
jcxiao <chang.x1994@gmail.com>
2021-03-19 15:55:34 -0400

    Added sorting functionality

--- src/components/InfoList.js
+++ src/components/InfoList.js
@@ -4,22 +4,32 @@ function PersonEntry(props) {
 
   const [displayedList, setDisplayedList] = useState([]);
 
-  // re-render list on reload
-  useEffect(() => {
-    setDisplayedList(props.data);
-  }, [props.data])
-
   // apply filter on list
   useEffect(() => {
-    console.log("Applying a filter:", props);
-    // do things
-  }, [props])
+    let filteredList = props.data;
+    // sort list by first name
+    if (props.sortName) filteredList.sort((a,b) => {
+      let [a1, b1] = [a.name.first.toLowerCase(), b.name.first.toLowerCase()];
+      return a1.localeCompare(b1);
+    })
+    // sort list by city name
+    if (props.sortCity) filteredList.sort((a,b) => {
+      let [a1, b1]= [a.location.city.toLowerCase(), b.location.city.toLowerCase()];
+      return a1.localeCompare(b1);
+    })
+    // hide entries that are male
+    if (!props.showMale) filteredList = filteredList.filter(entry => entry.gender !== "male");
+    // hide entries that are female
+    if (!props.showFemale) filteredList = filteredList.filter(entry => entry.gender !== "female");
+    // generate new displayed list on every change
+    setDisplayedList([...filteredList]);
+  }, [props]);
 
   return(
     <div className="row">
-      {displayedList.map(person => {
+      {displayedList.map((person,idx) => {
         return (
-          <div className="col-6 col-md-3 col-lg-2 card text-start">
+          <div className="col-6 col-md-3 col-lg-2 card text-start" key={idx}>
             <div className="card-body">
               <h5 className="card-title">{person.name.first} {person.name.last}</h5>
               <p className="card-text">{person.gender}</p>
--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -10,25 +10,22 @@ function SearchForm(props) {
   }
 
   function flickToggle(e) {
-    console.log(e.target.checked);
     // update filter
     switch (e.target.id) {
       case "sortByName": props.setSortName(e.target.checked); break;
       case "sortByCity": props.setSortCity(e.target.checked); break;
-      case "showMale": props.showMale(e.target.checked); break;
-      case "showFemale": props.showFemale(e.target.checked); break;
+      case "showMale": props.setShowMale(e.target.checked); break;
+      case "showFemale": props.setShowFemale(e.target.checked); break;
       default: console.log("unexpected error occurred while flicking toggles");
     }
-    // toggle off if on
-    if (e.target.checked) e.target.checked = false;
   }
 
   return(
     <form onSubmit={e => newSearch(e)} className="search-form">
       {/* Search bar for number of results */}
-      <label for="resultsNum" class="form-label">Number of results</label>
+      <label htmlFor="resultsNum" className="form-label">Number of results</label>
       <div className="input-group mb-3">
-        <input type="number" class="form-control" id="resultsNum" placeholder="Enter a number" />
+        <input type="number" className="form-control" id="resultsNum" placeholder="Enter a number" />
         <button type="submit" className="btn btn-primary">Search</button>
       </div>
       {/* Sort by checkboxes */}
@@ -36,11 +33,11 @@ function SearchForm(props) {
         <span className="mb-2 col-auto">Sort By</span>
         <div className="form-check col-auto mb-3">
           <input className="form-check-input" type="checkbox" id="sortByName" onClick={e => flickToggle(e)}/>
-          <label className="form-check-label" for="sortByName">Last Name</label>
+          <label className="form-check-label" htmlFor="sortByName">First name</label>
         </div>
         <div className="form-check col-auto mb-3">
           <input className="form-check-input" type="checkbox" id="sortByCity" onClick={e => flickToggle(e)}/>
-          <label className="form-check-label" for="sortByName">City name</label>
+          <label className="form-check-label" htmlFor="sortByName">City name</label>
         </div>
       </div>
       {/* Filter by checkboxes */}
@@ -48,11 +45,11 @@ function SearchForm(props) {
         <span className="mb-2 col-auto">Display </span>
         <div className="form-check col-auto mb-3">
           <input className="form-check-input" type="checkbox" id="showMale" defaultChecked={true} onClick={e => flickToggle(e)}/>
-          <label className="form-check-label" for="sortByName">Male</label>
+          <label className="form-check-label" htmlFor="sortByName">Male</label>
         </div>
         <div className="form-check col-auto mb-3">
           <input className="form-check-input" type="checkbox" id="showFemale" defaultChecked={true} onClick={e => flickToggle(e)}/>
-          <label className="form-check-label" for="sortByName">Female</label>
+          <label className="form-check-label" htmlFor="sortByName">Female</label>
         </div>
       </div>
     </form>
